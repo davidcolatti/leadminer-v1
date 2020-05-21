555011543fe72cf36641b4fb6f11d92aef8c326d
DavidColatti <davidcolatti@gmail.com>
2020-05-21 11:22:01 -0400

    landing css

--- frontend/src/components/landingpage/LandingPage.js
+++ frontend/src/components/landingpage/LandingPage.js
@@ -6,29 +6,31 @@ class LandingPage extends Component {
 			<div>
 				<div className="LandingPage">
 					<div className="landing-photo">
-						<strong>LeadMiner</strong>
-					</div>
-					<div className="features">
-						<div className="landing-feature">
-							<h2>Fresh Leads</h2>
-							<p>
-								Our software is created to mine small business information that is brand new, with
-								little to no web-presence.
-							</p>
+						<div className="landing-logo">
+							<strong>LeadMiner</strong>
 						</div>
-						<div className="landing-feature">
-							<h2>Close Deals</h2>
-							<p>
-								With having a consistent pipeline of new leads coming in, you'll find yourself
-								generating more revenue and closing more deals!
-							</p>
-						</div>
-						<div className="landing-feature">
-							<h2>Retain Clients</h2>
-							<p>
-								LeadMiner will let you become more organized with your workload and let you increase
-								client retention by keeping notes and following up to client requests.
-							</p>
+						<div className="features">
+							<div className="landing-feature">
+								<h2>Fresh Leads</h2>
+								<p>
+									Our software is created to mine small business information that is brand new, with
+									little to no web-presence.
+								</p>
+							</div>
+							<div className="landing-feature">
+								<h2>Close Deals</h2>
+								<p>
+									With having a consistent pipeline of new leads coming in, you'll find yourself
+									generating more revenue and closing more deals!
+								</p>
+							</div>
+							<div className="landing-feature">
+								<h2>Retain Clients</h2>
+								<p>
+									LeadMiner will let you become more organized with your workload and let you increase
+									client retention by keeping notes and following up to client requests.
+								</p>
+							</div>
 						</div>
 					</div>
 				</div>
